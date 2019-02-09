7ae6190d9fa0b9ec55905c057e0ebd1a7963a7d7
DURIAN\warapon.nn <eggzwc@gmail.com>
2019-02-10 00:36:59 +0900

    fix guntank

---
+++ 02/Assets/Scripts/CharControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour
+{
+    public GameObject ob;
+    public CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate() {
+        cc.Move(Vector3.forward);
+    }
+}
